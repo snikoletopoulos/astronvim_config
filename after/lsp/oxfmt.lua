65b127bcd9f5e1d8958c4f827abbebcf78a496ba
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2026-08-17 18:56:23 +0200

    Add configs for oxfmt and oxlint lsps

---
+++ after/lsp/oxfmt.lua
@@ -0,0 +1,14 @@
+---@type vim.lsp.Config | { settings?: lsp.oxfmt }
+return {
+	workspace_required = false,
+	root_dir = function(buffer, on_dir)
+		local filename = vim.api.nvim_buf_get_name(buffer)
+		local start_path = filename ~= "" and vim.fs.dirname(filename) or vim.uv.cwd()
+		if not start_path then return end
+
+		on_dir(
+			vim.fs.root(start_path, { ".oxfmtrc.json", ".oxfmtrc.jsonc", "oxfmt.config.ts", ".git" })
+				or start_path
+		)
+	end,
+}
